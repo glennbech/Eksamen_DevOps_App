6638347b68b8214e0cfe82cf7152bd181caab8d9
joakimstolen <joakimstoelen@hotmail.com>
2020-11-13 13:27:57 +0100

    Added get and put endpoints and tests for get and put endpoint of cards

--- src/main/kotlin/com/example/Eksamen/PathController.kt
+++ src/main/kotlin/com/example/Eksamen/PathController.kt
@@ -1,10 +1,22 @@
 package com.example.Eksamen
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.example.Eksamen.db.CardService
+import com.example.Eksamen.dto.CardCopyDto
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.apache.coyote.Response
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
+@Api(value = "/api/cards", description = "Operations on cards")
+@RequestMapping(
+        path = ["/api/cards"],
+        produces = [(MediaType.APPLICATION_JSON_VALUE)]
+)
 @RestController
-class PathController {
+class PathController (private val cardService: CardService) {
+
     @GetMapping(path = ["/"])
     fun home() : String{
         return "Welcome to home page, will this be visible?"
@@ -29,4 +41,26 @@ class PathController {
     fun page4() : String{
         return "Does page 1 work?"
     }
+
+    //@ApiOperation("Retrieve card collection information for a specific user")
+    @GetMapping(path = ["/{name}"])
+    fun getCardInfo(@PathVariable("name") cardName: String) : ResponseEntity<Void>{
+
+        val card = cardService.findByIdEager(cardName)
+        if(card == null){
+            return ResponseEntity.status(400).build()
+        }
+
+        return ResponseEntity.status(200).build()
+    }
+
+
+    @PutMapping(path = ["/{name}"])
+    fun createMonster(@PathVariable("name") cardName: String): ResponseEntity<Void> {
+        val ok = cardService.addNewCard(cardName)
+        return if (!ok) ResponseEntity.status(400).build()
+        else ResponseEntity.status(201).build()
+    }
+
+
 }
--- src/test/kotlin/com/example/Eksamen/PathControllerTest.kt
+++ src/test/kotlin/com/example/Eksamen/PathControllerTest.kt
@@ -1,9 +1,15 @@
 package com.example.Eksamen
 
+import com.example.Eksamen.db.CardRepository
+import com.example.Eksamen.db.CardService
 import io.restassured.RestAssured
 import org.hamcrest.Matchers
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
@@ -19,13 +25,46 @@ class PathControllerTest {
     @LocalServerPort
     protected var port = 0
 
+    @Autowired
+    private lateinit var cardService: CardService
+
+    @Autowired
+    private lateinit var cardRepository: CardRepository
+
     @PostConstruct
     fun init(){
         RestAssured.baseURI = "http://localhost"
         RestAssured.port = port
+        RestAssured.basePath = "/api/cards"
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
     }
 
+    @BeforeEach
+    fun initTest(){
+        cardRepository.deleteAll()
+    }
+
+    @Test
+    fun testGetCard(){
+        val id = "foo";
+        cardService.addNewCard(id)
+
+        RestAssured.given().get("/$id")
+                .then()
+                .statusCode(200)
+    }
+
+    @Test
+    fun testCreateCard(){
+        val id = "foo";
+        //cardService.addNewCard(id)
+
+        RestAssured.given().put("/$id")
+                .then()
+                .statusCode(201)
+
+        assertTrue(cardRepository.existsById(id))
+    }
 
     @Test
     fun testGet200Page1(){
@@ -56,9 +95,11 @@ class PathControllerTest {
     }
 
     @Test
-    fun testGet404OnUnknownAPI(){
+    fun testGet400OnUnknownAPI(){
         RestAssured.given().get("/doesNotExist")
                 .then()
-                .statusCode(404)
+                .statusCode(400)
     }
+
+
 }
